c9a32f1db354d4a5a1c6272e354c241356a652ec
Ashish Kumar <ashish.b.kumar@rakuten.com>
2023-01-01 22:25:48 +0530

    update styling logic and change track

--- src/utils/common/data.ts
+++ src/utils/common/data.ts
@@ -1,35 +1,35 @@
 export const metaData = {
-  title: 'Aasman Ke Neeche',
-  artist: 'Kishore Kumar',
-  album: 'Jewel Thief',
+  title: 'Apna Bana Le',
+  artist: 'Arijit Singh, Sachin-Jigar',
+  album: 'Bhediya',
   artwork: [
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '96x96',
       type: 'image/png',
     },
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '128x128',
       type: 'image/png',
     },
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '192x192',
       type: 'image/png',
     },
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '256x256',
       type: 'image/png',
     },
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '384x384',
       type: 'image/png',
     },
     {
-      src: 'https://c.saavncdn.com/784/Jewel-Thief-Hindi-1967-20200901153934-500x500.jpg',
+      src: 'https://c.saavncdn.com/815/Bhediya-Hindi-2022-20221206124543-500x500.jpg',
       sizes: '512x512',
       type: 'image/png',
     },
