3eb1f05feb0ab5563b89dd81a170aa7edc0dbae9
Ashish Kumar <ashish.b.kumar@rakuten.com>
2023-01-07 02:29:14 +0530

    another version of progress Icon

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import { notifierState } from '../utils/common/notifier';
 import ProgressIcon from './ProgressIcon';
+import ProgressIndicator from './ProgressIndicator';
 import Volume from './Volume';
 
 const Home = () => {
@@ -7,7 +8,8 @@ const Home = () => {
   return (
     <div className='flex justify-center items-center h-screen bg-black text-gray-300'>
       <div className='flex flex-col justify-center items-center gap-4'>
-        <ProgressIcon size='LG' />
+        {/* <ProgressIcon size='LG' /> */}
+        <ProgressIndicator />
         <Volume />
       </div>
       {/* <Progress /> */}
