2eccbacfd0f8611305aeb0ca9eaadcaf72b6ff3e
Ashish Kumar <ashish.kmr@outlook.com>
2023-02-21 01:45:40 +0530

    minor changes to audio

--- src/modules/audio/audio.ts
+++ src/modules/audio/audio.ts
@@ -1,9 +1,8 @@
 import { checkValidWindow } from '../../helpers/validators';
-import { metaData } from '../../utils/common/data';
 import attachAudioEventListeners from './eventListeners';
 import {
   attachMediaSessionHandlers,
-  updateMetaData,
+  updateMetaData
 } from './mediaSessionHandler';
 
 let audioInstance: HTMLAudioElement;
@@ -18,15 +17,22 @@ class AudioPlayer {
     audioInstance = this.audio;
   }
 
-  set updateSource(source: string) {
-    audioInstance.src = source;
-  }
-
   static getAudioInstance() {
     return audioInstance;
   }
 
-  play(metaData?: any) {
+  updateSource(source: string) {
+    audioInstance.src = source;
+  }
+
+  addMedia(source: string) {
+    this.reset();
+    this.updateSource(source);
+  }
+
+  play(source: string, metaData?: any) {
+    this.reset();
+    this.updateSource(source);
     const isSourceAvailable: boolean = audioInstance.src !== '';
     if (isSourceAvailable && audioInstance.HAVE_FUTURE_DATA) {
       audioInstance.play().then((_) => {
@@ -46,6 +52,10 @@ class AudioPlayer {
     audioInstance.pause();
   }
 
+  stop() {
+    this.reset();
+  }
+
   reset() {
     audioInstance.pause();
     audioInstance.currentTime = 0;
@@ -55,9 +65,9 @@ class AudioPlayer {
     attachAudioEventListeners();
   }
 
-  static updateMetaData(data: any) {
+  static updateMetaData(metaData: any) {
     if (checkValidWindow) {
-      updateMetaData(data);
+      updateMetaData(metaData);
     } else {
       throw new Error('window not found or undefined');
     }
